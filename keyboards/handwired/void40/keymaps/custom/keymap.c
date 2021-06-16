36247d4c7a944d20abeefe688d4621f7378dba7c
Jorge Ibacache <jo.ibacache@gmail.com>
2021-06-16 16:20:49 -0400

    typo fix

--- keyboards/handwired/void40/keymaps/custom/keymap.c
+++ keyboards/handwired/void40/keymaps/custom/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_mit(
     KC_ESC,     KC_Q,     KC_W,     KC_E,     KC_R,       KC_T, KC_Y,   KC_U,       KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_TAB,     KC_A,     KC_S,     KC_D,     KC_F,       KC_G, KC_H,   KC_J,       KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
-    KC_LSF,  KC_Z,     KC_X,     KC_C,     KC_V,       KC_B, KC_N,   KC_M,       KC_COMM,  KC_DOT,   KC_UP,    KC_SFTENT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,       KC_B, KC_N,   KC_M,       KC_COMM,  KC_DOT,   KC_UP,    KC_SFTENT,
     KC_NO,      KC_LGUI,  KC_LCTL,  KC_LALT,  MO(_LOWER),       KC_SPC, MO(_RAISE), KC_SLSH,  KC_LEFT,  KC_DOWN,  KC_RGHT
 ),
 
