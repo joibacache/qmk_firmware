2fba8070589e257ccdeeea8e09abd7d9a5f893a1
Jorge Ibacache <jo.ibacache@gmail.com>
2021-07-26 12:11:36 -0400

    added space_fn layer

--- keyboards/contra/keymaps/jorge/keymap.c
+++ keyboards/contra/keymaps/jorge/keymap.c
@@ -24,7 +24,8 @@ enum planck_layers {
   _COLEMAK,
   _LOWER,
   _RAISE,
-  _ADJUST
+  _ADJUST,
+  _SPACE_FN
 };
 
 enum planck_keycodes {
@@ -44,6 +45,10 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_TOGGLE_COLEMAK] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_NO, _COLEMAK),
 };
 
+#define SPACE_FN LT(_SPACE_FN, KC_SPC)
+#define VS_PREV RCTL(KC_MINS)
+#define VS_NEXT RCS(KC_MINS)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty
@@ -61,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,     KC_Q,     KC_W,     KC_E,     KC_R,       KC_T, KC_Y,   KC_U,       KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_TAB,     KC_A,     KC_S,     KC_D,     KC_F,       KC_G, KC_H,   KC_J,       KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
     KC_LSFT,    KC_Z,     KC_X,     KC_C,     KC_V,       KC_B, KC_N,   KC_M,       KC_COMM,  KC_DOT,   KC_UP,    KC_SFTENT,
-    TD(TD_TOGGLE_COLEMAK),      KC_LGUI,  KC_LCTL,  KC_LALT,  MO(_LOWER),    KC_SPC,    MO(_RAISE), KC_SLSH,  KC_LEFT,  KC_DOWN,  KC_RGHT
+    TD(TD_TOGGLE_COLEMAK),      KC_LGUI,  KC_LCTL,  KC_LALT,  MO(_LOWER),    SPACE_FN,    MO(_RAISE), KC_SLSH,  KC_LEFT,  KC_DOWN,  KC_RGHT
 ),
 
   /* Colemak 
@@ -90,14 +95,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * | Caps |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | | Home | Vol+ | End  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
-   * |      |      |      |      |      |     Play    |      | LCtrl| Prev | Vol- | Next |
+   * |      |      |      |      |      |     Play    |      |      | Prev | Vol- | Next |
    * `-----------------------------------------------------------------------------------'
    */
   [_LOWER] = LAYOUT_planck_mit(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,     KC_ASTR,    KC_LPRN,    KC_RPRN,    KC_TRNS,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,     KC_PLUS,    KC_LCBR,    KC_RCBR,    KC_PIPE,
     KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS),  S(KC_NUBS), KC_HOME,    KC_VOLU,    KC_END,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_MPLY,     KC_TRNS,     KC_LCTL,  KC_MPRV  ,    KC_VOLD,    KC_MNXT
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_MPLY,     KC_TRNS,     KC_NO,  KC_MPRV  ,    KC_VOLD,    KC_MNXT
   ),
 
   /* Raise
@@ -108,14 +113,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Caps |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 | Del  | Bksp |Pg Up | Vol+ |Pg Dn |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |     Play    |      |      | Prev | Vol- | Next |
+ * |      |      |      |      |      |     Play    |      |LCtrl| Prev | Vol- | Next |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_planck_mit(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_TRNS,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     KC_CAPS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC, KC_PGUP, KC_VOLU, KC_PGDN,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_MPLY,     KC_TRNS, KC_NO, KC_MPRV, KC_VOLD, KC_MNXT
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,      KC_MPLY,     KC_TRNS, KC_LCTL, KC_MPRV, KC_VOLD, KC_MNXT
 ),
 
   /* Adjust (Lower + Raise)
@@ -134,7 +139,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, AG_NORM, AG_SWAP, QWERTY,  COLEMAK, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
-  )
+  ),
+
+  /* Space fn
+  * ,-----------------------------------------------------------------------------------.
+  * |PRNT SC|      |  UP  |      |      |      |      |      |      |      |      |SLEEP |
+  * |-------+------+------+------+------+-------------+------+------+------+------+------|
+  * |       | LEFT | DOWN | RIGHT|      |      |      | LEFT | DOWN |  UP  | RIGHT|      |
+  * |-------+------+------+------+------+------|------+------+------+------+------+------|
+  * |       |      |      |      |      |VSPREV|VSNEXT|      |      |      |      |      |
+  * |-------+------+------+------+------+------+------+------+------+------+------+------|
+  * |       |      |      |      |      |             |      | MUTE |VOLDWN|VOL UP|      |
+  * `-----------------------------------------------------------------------------------'
+  */
+  [_SPACE_FN] = LAYOUT_planck_mit(
+      LSG(KC_S),  XXXXXXX, KC_UP,    XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,    KC_SLEP,
+      XXXXXXX,  KC_LEFT, KC_DOWN,  KC_RIGHT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LEFT, KC_DOWN,  KC_UP,       KC_RIGHT,   XXXXXXX,
+      XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,  VS_PREV,  VS_NEXT,  XXXXXXX, XXXXXXX,  XXXXXXX,     XXXXXXX,    XXXXXXX,
+      XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,  _______,  XXXXXXX, KC__MUTE, KC__VOLDOWN, KC__VOLUP,  XXXXXXX
+  ),
 
 };
 
