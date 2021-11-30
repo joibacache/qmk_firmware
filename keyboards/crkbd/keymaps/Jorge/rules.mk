8f45aaa11172abcd2807f95fbca13973a02956b7
Jorge Ibacache <jo.ibacache@gmail.com>
2021-11-30 10:15:21 -0300

    still doing some adjunstments on my corne

--- keyboards/crkbd/keymaps/Jorge/rules.mk
+++ keyboards/crkbd/keymaps/Jorge/rules.mk
@@ -1,12 +1,20 @@
-BOOTMAGIC_ENABLE = no
-OLED_ENABLE = no
-OLED_DRIVER = SSD1306
+# BOOTMAGIC_ENABLE = no
+# OLED_ENABLE = no
+# OLED_DRIVER = SSD1306
 
-# RGBLIGHT_ENABLE = no
+# # RGBLIGHT_ENABLE = no
+# # RGB_MATRIX_ENABLE = yes
+
+# BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+# RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+# AUDIO_ENABLE = no           # Audio output
 # RGB_MATRIX_ENABLE = yes
+# RGB_MATRIX_DRIVER = SK6812
 
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = SK6812
+
+RGBLIGHT_ENABLE    = no
+RGB_MATRIX_ENABLE  = yes
+MOUSEKEY_ENABLE    = no
+NKRO_ENABLE        = yes
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
