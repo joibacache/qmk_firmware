44beeec4c26ab03f81ae2655dbc97226571d6d00
Jorge Ibacache <jo.ibacache@gmail.com>
2021-11-30 08:59:43 -0300

    added my own keymap for crkbd

---
+++ keyboards/crkbd/keymaps/Jorge/rules.mk
@@ -0,0 +1,12 @@
+BOOTMAGIC_ENABLE = no
+OLED_ENABLE = no
+OLED_DRIVER = SSD1306
+
+# RGBLIGHT_ENABLE = no
+# RGB_MATRIX_ENABLE = yes
+
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = SK6812
