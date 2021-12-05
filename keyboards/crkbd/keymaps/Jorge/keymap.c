bf45dbe35c6bab9303cf67ae061defa6fdb19d9f
Jorge Ibacache <jo.ibacache@gmail.com>
2021-12-05 12:30:22 -0300

    mods on keymap and minor refactoring on process_redord_user func

--- keyboards/crkbd/keymaps/Jorge/keymap.c
+++ keyboards/crkbd/keymaps/Jorge/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q, KC_W, KC_E, KC_R, KC_T,           KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
     KC_TAB,  KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
     KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,           KC_N, KC_M, KC_COMM, KC_DOT, KC_NO,   KC_SFTENT,
-    KC_LALT, MO(_LOWER), SFT_T(KC_SPC),              SPACE_FN,MO(_RAISE), KC_SLSH  
+    KC_LALT, LOWER, SFT_T(KC_SPC),              SPACE_FN,RAISE, KC_SLSH  
 ),
 
     
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2, KC_F3,   KC_F4,  KC_F5,            KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
     KC_CAPS, KC_F7,   KC_F8, KC_F9,   KC_F10, KC_F11,           KC_F12,  KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-    KC_LALT, MO(_LOWER), KC_NO,                                 SPACE_FN,MO(_RAISE), KC_NO
+    KC_LALT, LOWER, KC_NO,                                 SPACE_FN,RAISE, KC_NO
 ),
 
     
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV,  KC_1,  KC_2, KC_3,   KC_4,   KC_5,         KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5,        KC_F6,  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     KC_CAPS, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,       KC_F12, KC_DEL,  KC_BSPC, KC_PGUP, KC_VOLU, KC_PGDN,
-    KC_LALT, MO(_LOWER), KC_NO,                         SPACE_FN, MO(_RAISE), KC_NO
+    KC_LALT, LOWER, KC_NO,                         SPACE_FN, RAISE, KC_NO
 ),
 
     
@@ -217,60 +217,38 @@ bool oled_task_user(void) {
     return false;
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    set_keylog(keycode, record);
-  }
-    
-  switch (keycode) {
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-  }
-  return true;
-}
-#else
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case LOWER:
-      if (record->event.pressed) {
-        layer_on(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_LOWER);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case RAISE:
-      if (record->event.pressed) {
-        layer_on(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      } else {
-        layer_off(_RAISE);
-        update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-  }
-  return true;
-}
-
 #endif // OLED_ENABLE
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) 
+{
+  #ifdef OLED_ENABLE
+    if (record->event.pressed) {
+      set_keylog(keycode, record);
+    }
+  #endif
+
+  switch (keycode) {
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+    
+    case RAISE:
+      if (record->event.pressed) {
+      layer_on(_RAISE);
+      update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+      layer_off(_RAISE);
+      update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+    
+    default:
+      return true; // Process all other keycodes normally
+  }
+}
