51a6dd642c8a0aadebac0872618b7c5bf5355237
Jorge Ibacache <jo.ibacache@gmail.com>
2022-01-21 22:01:24 -0300

    keymap adjustment

--- keyboards/crkbd/keymaps/Jorge/keymap.c
+++ keyboards/crkbd/keymaps/Jorge/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,  KC_W, KC_E, KC_R, KC_T,                 KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_BSPC,
     KC_TAB,  KC_A,  KC_S, KC_D, KC_F, KC_G,                 KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT,
     KC_LGUI, KC_Z,  KC_X, KC_C, KC_V, KC_B,                 KC_N, KC_M, KC_COMM, KC_DOT, KC_NO,   KC_SFTENT,
-    KC_LALT, LOWER, SHIFT_SPC,                              SPACE_FN, RAISE, KC_SLSH  
+    KC_LALT, LOWER, KC_NO,                              SPACE_FN, RAISE, KC_SLSH  
 ),
 
     
@@ -138,8 +138,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SPACE_FN] = LAYOUT_split_3x6_3(
     LSG(KC_S), RGB_HUI, RGB_HUD, KC_HOME, KC_UP,   KC_END,        KC_HOME, KC_UP,   KC_END,   KC_NO,   KC_NO,   KC_BSPC,
     KC_DEL,    RGB_SAI, RGB_SAD, KC_LEFT, KC_DOWN, KC_RIGHT,      KC_LEFT, KC_DOWN, KC_RIGHT, KC_MUTE, KC_VOLU, KC_NO,
-    KC_LCTL,   RGB_VAI, RGB_VAD, KC_NO,   KC_NO,   RGB_MOD,       VS_PREV, KC_MPLY, KC_MPRV,  KC_MNXT, KC_VOLD, KC_NO,
-    KC_LALT,   KC_LGUI, KC_LSFT,                                    SPACE_FN, RGB_TOG, RGB_MOD
+    KC_LSFT,   RGB_VAI, RGB_VAD, KC_NO,   KC_NO,   RGB_MOD,       VS_PREV, KC_MPLY, KC_MPRV,  KC_MNXT, KC_VOLD, KC_NO,
+    KC_LALT,   KC_LGUI, KC_LCTL,                                    SPACE_FN, RGB_TOG, RGB_MOD
 )
 };
 
